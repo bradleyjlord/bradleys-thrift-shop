3c311711e9c53750ae74bb42aa96d13a2d844309
Bradley Jlord <bradleyjlord721@gmail.com>
2024-10-14 22:57:36 +0800

    first commi

--- main.js
+++ main.js
@@ -1,2 +1,36 @@
-import './style.css'
+let currentIndex = 0;
+const images = document.querySelectorAll('.cover-image img');
+const totalImages = images.length;
+const coverImageContainer = document.querySelector('.cover-image');
+const prevButton = document.querySelector('.prev');
+const nextButton = document.querySelector('.next');
 
+function updateSlide(index) {
+  coverImageContainer.style.transform = `translateX(-${index * 100 / totalImages}%)`;
+}
+
+
+let slideInterval = setInterval(() => {
+  currentIndex = (currentIndex + 1) % totalImages;
+  updateSlide(currentIndex);
+}, 7000);
+
+nextButton.addEventListener('click', () => {
+  currentIndex = (currentIndex + 1) % totalImages;
+  updateSlide(currentIndex);
+  resetInterval();
+});
+
+prevButton.addEventListener('click', () => {
+  currentIndex = (currentIndex - 1 + totalImages) % totalImages;
+  updateSlide(currentIndex);
+  resetInterval();
+});
+
+function resetInterval() {
+  clearInterval(slideInterval);
+  slideInterval = setInterval(() => {
+    currentIndex = (currentIndex + 1) % totalImages;
+    updateSlide(currentIndex);
+  }, 10000);
+}
