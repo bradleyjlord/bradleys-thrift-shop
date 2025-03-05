9d579bb20b812c838e1d40d1378853a75a14e57d
Bradley Jlord <bradleyjlord073@gmail.com>
2025-03-05 18:06:53 +0800

    add featured section and remove slider functionality

--- main.js
+++ main.js
@@ -2,13 +2,6 @@ document.addEventListener("DOMContentLoaded", () => {
   const burger = document.querySelector(".burger")
   const nav = document.querySelector(".nav-links")
   const navLinks = document.querySelectorAll(".nav-links li")
-  const coverImageContainer = document.querySelector(".cover-image")
-  const prevButton = document.querySelector(".prev")
-  const nextButton = document.querySelector(".next")
-
-  let currentIndex = 0
-  const totalImages = document.querySelectorAll(".cover-image img").length
-  let slideInterval
 
   function toggleNav() {
     nav.classList.toggle("active")
@@ -27,40 +20,5 @@ document.addEventListener("DOMContentLoaded", () => {
   navLinks.forEach((link) => {
     link.addEventListener("click", closeNav)
   })
-
-  function updateSlide(index) {
-    coverImageContainer.style.transform = `translateX(-${(index * 100) / totalImages}%)`
-  }
-
-  function nextSlide() {
-    currentIndex = (currentIndex + 1) % totalImages
-    updateSlide(currentIndex)
-  }
-
-  function prevSlide() {
-    currentIndex = (currentIndex - 1 + totalImages) % totalImages
-    updateSlide(currentIndex)
-  }
-
-  function startSlideInterval() {
-    slideInterval = setInterval(nextSlide, 7000)
-  }
-
-  function resetInterval() {
-    clearInterval(slideInterval)
-    startSlideInterval()
-  }
-
-  nextButton.addEventListener("click", () => {
-    nextSlide()
-    resetInterval()
-  })
-
-  prevButton.addEventListener("click", () => {
-    prevSlide()
-    resetInterval()
-  })
-
-  startSlideInterval()
 })
 
